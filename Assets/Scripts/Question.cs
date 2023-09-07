a2c08bf6bf8b79130f4a4301fe5ecf395dce0d6a
eocnddlfma <85099835+eocnddlfma@users.noreply.github.com>
2023-09-07 11:18:57 +0900

    확인좀 해줘

--- Assets/Scripts/Question.cs
+++ Assets/Scripts/Question.cs
@@ -7,13 +7,17 @@ public class Question : MonoBehaviour
 {
     [SerializeField] [TextArea(3, 5)]
     public string[] question;
+    public string[] explanation;
     public TMP_Text questionText;
     public bool[] isAnswer = { true, true, true, true, false, true, false, false };
     public ButtonScript buttonScript;
-    
+
     public void SetQuestion(int index)
     {
         questionText.text = question[index];
-        buttonScript.anwsers = !isAnswer[index];
+    }
+    public void SetExplanation(int index)
+    {
+        questionText.text = explanation[index];
     }
 }
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -15,13 +15,57 @@ public class Timer : MonoBehaviour
     int questionIndex = 0;
     ButtonScript buttonScript;
     [SerializeField] private Image xImage;
+    [SerializeField] private Image timeoverImage;
     [SerializeField] private TMP_Text oImage;
     public Image[] hpImage;
     public int hp = 2;
     public TMP_Text Text;
     public TMP_Text leftTime;
     public GameObject blockPanel;
-    
+    float timer;
+    bool currentAnswer, input=false, timeout = false;
+
+    public TMP_Text talkText;
+    public TMP_Text nameText;
+    public Image student;
+    public Image teacher;
+    public int textSpeed;
+    public GameObject talkpanel;
+
+    IEnumerator correctChat()
+    {
+        talkpanel.SetActive(true);
+        teacher.color = new Color(1, 1, 1, 1);
+        yield return StartCoroutine(NormalChat("선생", "오!"));
+        yield return StartCoroutine(NormalChat("선생", "내 학생이 이 문제를 맞추다니?!"));
+        yield return StartCoroutine(NormalChat("선생", "뿌듯하구만."));
+        teacher.color = new Color(1, 1, 1, 0.5f);
+        talkpanel.SetActive(false);
+        StartCoroutine(nextProblem());
+    }
+    IEnumerator wrongChat()
+    {
+        talkpanel.SetActive(true);
+        student.color = new Color(1, 1, 1, 1);
+        yield return StartCoroutine(NormalChat("학생", "윽!"));
+        yield return StartCoroutine(NormalChat("학생", "내가 고작 이런 문제를 틀리다니?!"));
+        yield return StartCoroutine(NormalChat("학생", "수치스럽다..."));
+        student.color = new Color(1, 1, 1, 0.5f);
+        talkpanel.SetActive(false);
+        StartCoroutine(nextProblem());
+    }
+
+    IEnumerator overtimeChat()
+    {
+        talkpanel.SetActive(true);
+        student.color = new Color(1, 1, 1, 1);
+        yield return StartCoroutine(NormalChat("학생", "윽!"));
+        yield return StartCoroutine(NormalChat("학생", "내가 고작 이런 문제를 풀지 못하다니?!"));
+        yield return StartCoroutine(NormalChat("학생", "수치스럽다..."));
+        student.color = new Color(1, 1, 1, 0.5f);
+        talkpanel.SetActive(false);
+        StartCoroutine(nextProblem());
+    }
 
     private void Awake()
     {
@@ -42,7 +86,7 @@ public class Timer : MonoBehaviour
             yield return new WaitForSeconds(time/5);
         }
         blockPanel.SetActive(false);
-        StartCoroutine(TimerCoroutine(time:timerTime));
+        StartCoroutine(Gameflow());
     }
     void HPUpdate()
     {
@@ -54,24 +98,142 @@ public class Timer : MonoBehaviour
             hpImage[hp].gameObject.SetActive(false);
     }
 
+    public void OAnswer()
+    {
+        currentAnswer = true;
+        input = true;
+    }
+    public void XAnswer()
+    {
+        currentAnswer = false;
+        input = true;
+    }
+
     IEnumerator Gameflow()
     {
+        question.SetQuestion(questionIndex);//문제 제시
+        timer = timerTime;
+        StartCoroutine("Problemtimer");
+
+        while(!timeout && !input)
+        {
+            yield return null;
+        }
+        StopCoroutine("Problemtimer");
+
+
+        if (input && currentAnswer == question.isAnswer[questionIndex])
+        {
+            oImage.gameObject.SetActive(true);
+            Text.text = "정답";
+            StartCoroutine(correctChat());
+        }
+        else if (input && currentAnswer != question.isAnswer[questionIndex])
+        {
+
+            xImage.gameObject.SetActive(true);
+            StartCoroutine(wrongChat());
+            hp--;
+            Text.text = "오답";
+            HPUpdate();
+        }
+        else if (timeout)
+        {
+            timeoverImage.gameObject.SetActive(true);
+            StartCoroutine(overtimeChat());
+            hp--;
+            Text.text = "시간초과";
+            HPUpdate();
+        }
+        yield return new WaitForSeconds(1f);
+
+        Text.text = "";
+
+        input = false;
+        timeout = false;
+        oImage.gameObject.SetActive(false);
+        xImage.gameObject.SetActive(false);
+        timeoverImage.gameObject.SetActive(false);
+
+        question.SetExplanation(questionIndex);
+
+        yield return new WaitForSeconds(3f);
         /*
         첫번째로 타이머를 작동시킴.
         만약에 입력이 들어오면 입력을 작동시키고 타이머 작동을 멈춤
-        타이머가 끝날때 까지 아무 입력이 없으면 타임아웃처리
+        타이머가 끝날때 까지 아무 입력이 없으면 타임아웃처리 
 
+        정답을 확인함
+        정답이면 O표시
+        틀리면 X표시
+        시간초과면 시간초과
+
+        이후 해설 보여줌
+
+        결과에 따른 대사
+
+        다음문제 제시
         */
-        yield return null;
     }
 
+    IEnumerator nextProblem()
+    {
+        questionIndex++;
+        if(questionIndex<8)
+        StartCoroutine(Gameflow());
+        yield return null;
+
+    }
+
+
+    IEnumerator Problemtimer()
+    {
+        while (timer > 0)
+        {
+            // 1초마다 타이머를 줄여준다.
+            timer -= Time.deltaTime;
+            // 타이머 이미지를 줄여준다.
+            timerImage.fillAmount = timer / timerTime;
+            // 1프레임 대기한다.
+            yield return null;
+        }
+        timerImage.fillAmount = timer / timerTime;
+    }
+
+    IEnumerator NormalChat(string narrator, string content)
+    {
+        nameText.text = narrator;
+        string output = "";
+        talkText.text = "";
+        for (int a = 0; a < content.Length; a++)
+        {
+            output += content[a];
+            talkText.text = output;
+            for (int b = 0; b < textSpeed; b++)
+            {
+                yield return null;
+                if (Input.anyKeyDown) break;
+            }
+            if (Input.anyKeyDown) break;
+
+        }
+        talkText.text = content;
+
+        while (true)
+        {
+            yield return null;
+            if (Input.anyKeyDown)
+            {
+                break;
+            }
+        }
+    }
 
     IEnumerator TimerCoroutine(float time)
     {
         a:
         question.SetQuestion(questionIndex);
         // 타이머 시간만큼 타이머 이미지를 줄여준다.
-        float timer = time;
         // 타이머가 0이 될 때까지 반복한다.
         while (timer > 0)
         {
